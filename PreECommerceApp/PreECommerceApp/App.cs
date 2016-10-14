e02edfe2637f969494d5470bc216181918723aca
Sergio <Sergio@SERGIO>
2016-10-14 15:56:54 -0500

    Login function (video 63)

--- PreECommerceApp/PreECommerceApp/App.cs
+++ PreECommerceApp/PreECommerceApp/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PreECommerceApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,8 @@ namespace PreECommerceApp
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new NavigationPage(new LoginPage());
+           
         }
 
         protected override void OnStart()
---
+++ PreECommerceApp/PreECommerceApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using PreECommerceApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PreECommerceApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            Padding = Device.OnPlatform(
+                new Thickness(10, 20, 10, 10),
+                new Thickness(10),
+                new Thickness(10)
+            );
+
+            loginButton.Clicked += loginButton_Clicked;
+        }
+
+        private async void loginButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userEntry.Text))
+            {
+                await DisplayAlert("Error", "You must enter an email", "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                await DisplayAlert("Error", "You must enter a password", "Accept");
+                return;
+            }
+            Login();
+        }
+
+        private async void Login()
+        {
+            waitActivityIndicator.IsRunning = true;
+
+            var loginRequest = new LoginRequest
+            {
+                Email = userEntry.Text,
+                Password = passwordEntry.Text,
+            };
+            var result = string.Empty;
+
+            try
+            {
+                var jsonRequest = JsonConvert.SerializeObject(loginRequest);
+                var httpContent = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://zulu-software.com");
+                var url = "/ECommerce/api/Users/Login";
+                var response = await client.PostAsync(url, httpContent);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    waitActivityIndicator.IsRunning = false;
+                    await DisplayAlert("Error", "Wrong user or password ", "Accept");
+                    passwordEntry.Text = string.Empty;
+                    passwordEntry.Focus();
+                    return;
+                }
+
+                result = await response.Content.ReadAsStringAsync();
+
+            }
+            catch (Exception ex)
+            {
+                waitActivityIndicator.IsRunning = false;
+                await DisplayAlert("Error", ex.Message, "Accept");
+                return;
+            }
+
+
+            var userResponse = JsonConvert.DeserializeObject<UserResponse>(result);
+            userResponse.Password = passwordEntry.Text;
+            waitActivityIndicator.IsRunning = false;
+            await Navigation.PushAsync(new MainPage(userResponse));
+        }
+    }
+}
